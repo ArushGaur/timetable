702ef2d3e7225538c171c6501c9a8acbd7fea0c8
Arush Gaur <98317460+ArushGaur@users.noreply.github.com>
2026-01-03 10:33:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,11 +10,34 @@ const FILES_TO_CACHE = [
   "/timetable/icon-512.jpg"
 ];
 
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
   self.clients.claim();
 });
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
